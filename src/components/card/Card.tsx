ff4f6f88c3cbe7b937f3f4487b55318f17e50fdd
Daniel von Boros <danielvonboros@gmail.com>
2025-02-12 00:12:34 +0100

    feat: add mockData for cards and controls

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,10 +1,32 @@
-import { Box, Paper } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { mockCardContent } from "./mockCardContent";
+import { useState } from "react";
 
 const Card = () => {
+  const [currentCard, setCurrentCard] = useState(0);
+
+  const goToPrevious = () => {
+    setCurrentCard((prevIndex) =>
+      prevIndex === 0 ? mockCardContent.length - 1 : prevIndex - 1
+    );
+  };
+
+  const goToNext = () => {
+    setCurrentCard((prevIndex) =>
+      prevIndex === mockCardContent.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const currentItem = mockCardContent[currentCard];
+
   return (
     <Paper sx={{ width: "350px", height: "500px" }}>
       <Box sx={{ my: "auto" }}>
-        This is an example card made of the Paper component
+        <Typography>{currentItem.text}</Typography>
+      </Box>
+      <Box>
+        <Button onClick={goToPrevious}> (-) </Button>
+        <Button onClick={goToNext}> (+) </Button>
       </Box>
     </Paper>
   );
---
+++ src/components/card/mockCardContent.ts
@@ -0,0 +1,17 @@
+export const mockCardContent = [
+    {
+        id: 1,
+        text: "No amount of guilt can change the past and no amount of anxiety can change the future.",
+        favorite: false,
+    },
+    {
+        id: 2,
+        text: "You are handling this situation better than your old self would have.",
+        favorite: false,
+    },
+    {
+        id: 3,
+        text: "You can't make somebody love you by giving them more of what they already don't appreciate.",
+        favorite: false,
+    },
+]
